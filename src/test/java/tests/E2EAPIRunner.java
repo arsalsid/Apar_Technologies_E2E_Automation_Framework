ea6ff0b3340969d107e8ffbb727410814e1dec40
SYSTEMS\marsalan.siddiqui <marsalan.siddiqui@systemsltd.com>
2025-01-09 15:06:43 +0500

    code clean

--- src/test/java/tests/E2EAPIRunner.java
+++ src/test/java/tests/E2EAPIRunner.java
@@ -7,6 +7,7 @@ import baseTest.PropertyReader;
 import com.aventstack.extentreports.ExtentTest;
 import com.aventstack.extentreports.Status;
 import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import utils.extentReports.ExtentManager;
@@ -17,8 +18,8 @@ import java.lang.reflect.Method;
 
 public class E2EAPIRunner {
 
-    @Test(dataProvider = "bookingDataProvider", description = "Create a new valid or invalid booking and GET ID of Details", priority = 1)
-    public void createValidAndInvalidDataAndGetIdWithDetails(String requestBodyPath, int expectedStatusCode) throws IOException, InterruptedException {
+    @Test(dataProvider = "positiveBookingDataProvider", description = "TC-001, Create a new valid booking and GET ID of Details", priority = 1)
+    public void createValidDataAndGetIdWithDetails(String requestBodyPath, int expectedStatusCode) throws IOException, InterruptedException {
 
         ExtentTest test = ExtentTestManager.startTest("Create New Booking API Test", "This test will create a new booking");
 
@@ -30,15 +31,13 @@ public class E2EAPIRunner {
             CreateBooking createBooking = new CreateBooking();
             String bookingId = createBooking.createNewBooking(requestBodyPath, expectedStatusCode);
 
-            if (bookingId != null) {
-                // Get booking details using the GET method
-                GetBooking getBooking = new GetBooking();
-                getBooking.getBookingById(bookingId,expectedStatusCode);
-                test.log(Status.PASS, "Booking created successfully with ID:" + bookingId);
-            } else {
-                // Handle failure cases (400, 500 errors)
-                test.log(Status.FAIL, "Booking creation failed with status code: " + expectedStatusCode);
-            }
+            Assert.assertNotNull(bookingId, "Booking ID should not be null for valid data");
+
+            // Get booking details using the GET method
+            GetBooking getBooking = new GetBooking();
+            getBooking.getBookingById(bookingId,expectedStatusCode);
+
+            test.log(Status.FAIL, "Valid booking created successfully with ID: " + expectedStatusCode);
 
         } catch (Exception e) {
             test.log(Status.FAIL, "Test failed due to: " + e.getMessage());
@@ -47,28 +46,69 @@ public class E2EAPIRunner {
             ExtentManager.createExtentReports().flush();
         }
     }
+    @Test(dataProvider = "negativeBookingDataProvider", description = "TC-002,Create a new invalid booking and GET ID of Details", priority = 2)
+    public void createInvalidBookingAndGetIDWithDetails(String requestBodyPath, int expectedStatusCode) throws IOException {
 
-    @DataProvider(name = "bookingDataProvider")
-    public Object[][] bookingData() {
-        return new Object[][]{
+        ExtentTest test = ExtentTestManager.startTest("Create Invalid Booking API Test", "This test will attempt to create an invalid booking");
 
-                // valid test case with 200 status code
-                {
-                        "src/test/resources/request_JSON/CreateBookingRequest.json",  // requestBodyPath
-                        200  // expectedStatusCode
-                },
+        try {
+            test.log(Status.INFO, "Starting the API request for creating an invalid booking");
 
-                //Invalid test case with 400 status code e.g.(missing required fields and incorrect data)
-                {
-                        "src/test/resources/request_JSON/CreateBookingNegativeRequest.json",  // requestBodyPath
-                        400  // expectedStatusCode
-                },
+            CreateBooking createBooking = new CreateBooking();
+            String bookingId = createBooking.createNewBooking(requestBodyPath, expectedStatusCode);
 
-                {
-                        "src/test/resources/request_JSON/CreateBookingEmptyRequest.json",  // requestBodyPath
-                        422  // expectedStatusCode
-                },
+            Assert.assertNull(bookingId, "Booking ID should be null for invalid data");
 
+            test.log(Status.PASS, "Handled invalid booking creation with expected status code: " + expectedStatusCode);
+        } catch (Exception e) {
+            test.log(Status.FAIL, "Test failed due to: "+e.getMessage());
+            throw e;
+        } finally {
+            ExtentManager.createExtentReports().flush();
+        }
+
+    }
+    @Test(dataProvider = "emptyBookingDataProvider", description = "TC-003, Create a booking with Empty data and GET ID of Details", priority = 3)
+    public void createBookingWithEmptyDataAndGetIDWithDetails(String requestBodyPath, int expectedStatusCode) throws IOException {
+
+        ExtentTest test = ExtentTestManager.startTest("Create Booking with Empty Data API Test","This test will attempt to create a booking with empty data");
+
+        try {
+            test.log(Status.INFO, "Starting the API request for creating a booking with empty data");
+
+            CreateBooking createBooking = new CreateBooking();
+            String bookingId = createBooking.createNewBooking(requestBodyPath, expectedStatusCode);
+
+            Assert.assertNull(bookingId, "Booking ID should be null for empty data");
+
+            test.log(Status.PASS, "Handled booking creation with empty data and expected status code: " + expectedStatusCode);
+        } catch (Exception e) {
+            test.log(Status.FAIL, "Test failed due to: "+e.getMessage());
+            throw e;
+        } finally {
+            ExtentManager.createExtentReports().flush();
+        }
+
+    }
+
+    @DataProvider(name = "positiveBookingDataProvider")
+    public Object[][] positiveBookingData() {
+        return new Object[][] {
+                {"src/test/resources/request_JSON/CreateBookingRequest.json", 200}
+        };
+    }
+
+    @DataProvider(name = "negativeBookingDataProvider")
+    public Object[][] negativeBookingData() {
+        return new Object[][] {
+                {"src/test/resources/request_JSON/CreateBookingNegativeRequest.json", 400}
+        };
+    }
+
+    @DataProvider(name = "emptyBookingDataProvider")
+    public Object[][] emptyBookingData() {
+        return new Object[][] {
+                {"src/test/resources/request_JSON/CreateBookingEmptyRequest.json", 422}
         };
     }
 
